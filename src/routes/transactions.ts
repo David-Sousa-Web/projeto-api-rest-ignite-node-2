52427e9aff287a898b82aeec51c6c455f1c1cffa
David Sousa <david01sousa@hotmail.com>
2023-09-11 15:27:45 -0300

    feat: create route get and getID

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -4,6 +4,24 @@ import { knex } from "../database";
 import { randomUUID } from "node:crypto";
 
 export async function transactionsRoutes(app: FastifyInstance) {
+  app.get("/", async () => {
+    const transactions = await knex("transactions").select();
+
+    return { transactions };
+  });
+
+  app.get("/:id", async (request) => {
+    const getTransactionParmsSchema = z.object({
+      id: z.string().uuid(),
+    });
+
+    const { id } = getTransactionParmsSchema.parse(request.params);
+
+    const transcation = await knex("transactions").where("id", id).first();
+
+    return { transcation };
+  });
+
   app.post("/", async (request, reply) => {
     const createTransactionBodySchema = z.object({
       title: z.string(),
