6bc1430256b8d76876551aaf9565b093bbfc683a
David Sousa <david01sousa@hotmail.com>
2023-09-12 17:38:14 -0300

    fix: title name

---
+++ db/migrations/20230912203430_update-transaction-tittle.ts
@@ -0,0 +1,13 @@
+import { Knex } from "knex";
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.alterTable("transactions", (table) => {
+    table.renameColumn("tittle", "title");
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.alterTable("transactions", (table) => {
+    table.dropColumn("title");
+  });
+}
